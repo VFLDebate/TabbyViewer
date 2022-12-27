9eefac2ae2f2523fabf8d7b199bbbfbc6e90460e
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2022-12-27 20:49:41 +0300

    Create Blazor Server App Test Project

--- Program.cs
+++ Program.cs
@@ -1,51 +1,25 @@
-using System.Net.Http;
-using TabbyViewer.Api;
-using TabbyViewer.Configuration;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
-namespace TabbyViewer
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        static async Task RunAsync(string[] args)
-        {
-            var client = new HttpClient();
-            Config config = ImportConfig.FromFile();
-            
-            client.BaseAddress = new Uri(config.Url);
-            client.DefaultRequestHeaders.Add("Authorization", $"Token {config.Token}");
-            
-            Ops.client = client;
-            Ops.config = config;
-            
-            var builder = WebApplication.CreateBuilder(args);
-            
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-            
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            
-            app.UseRouting();
-            
-            app.UseAuthorization();
-            
-            app.MapRazorPages();
-            
-            await app.RunAsync();
-        }
-        static void Main(string[] args)
-        {
-            RunAsync(args).GetAwaiter().GetResult();
-        }
-    }
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
